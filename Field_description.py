61c17d680fbc12ffbc537d33a2719fddb57a2343
Semoyn <106271169+Semyon49@users.noreply.github.com>
2024-07-09 17:15:20 +0300

    Init func

--- Field_description.py
+++ Field_description.py
@@ -6,3 +6,12 @@ auv = mur.mur_init()
 
 image = auv.get_image_bottom()
 height, width = image.shape[:2]
+
+def search_circle(image, ) -> bool:
+    ...
+
+def search_triangle(image, ) -> bool:
+    ...
+
+def search_square(image, ) -> bool:
+    ...
