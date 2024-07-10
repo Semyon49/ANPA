9804d88dafba1931199e94322355108af22e6f72
Semoyn <106271169+Semyon49@users.noreply.github.com>
2024-07-10 08:59:48 +0300

    Best config for search quadrilateral

--- Field_description.py
+++ Field_description.py
@@ -1,35 +1,41 @@
 import cv2 as cv
 import pymurapi as mur
-import time
+from time import sleep
 
 auv = mur.mur_init()
-hash_figure: list = []
-red: tuple = (0, 0, 255)
+blue:  tuple = (255, 0, 0)
+green: tuple = (0, 255, 0)
+red:   tuple = (0, 0, 255)
+
+
+def drawContur(img, conrour):
+    cv.drawContours(img, [conrour], 0, red, 5)
+    cv.imshow('', img)
+    cv.waitKey(1)
 
 def search_object(image) -> str:
+    hash_figure: list = []
     name_figure: dict[int, str] = {0: 'Circle', 3: 'Triangle', 4: 'Quadrilateral'}
 
     img  = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
-    hsv_low: tuple = (5, 100, 100)
-    hsv_max: tuple = (150, 255, 255)
+    hsv_low, hsv_max = (25, 100, 100), (150, 255, 255)
 
     mask = cv.inRange(img, hsv_low, hsv_max)
-
     contours, _ =cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     for conrour in contours:
         approx = cv.approxPolyDP(conrour, 0.01 * cv.arcLength(conrour, True), True)
-
-        if len(approx) in (1, 2): continue
-        cv.drawContours(image, [conrour], 0, red, 5)
-        cv.imshow('', image)
-        cv.waitKey(1)
-
-        if len(approx) < 5: return name_figure[len(approx)]
+        
+        try:
+            if name_figure[len(approx)] is None: continue
+            if len(approx) < 5: print(name_figure[len(approx)])
+            drawContur(img, conrour)
+        except  KeyError:
+            sleep(0.1)
     
 # Test
 while True:
-    time.sleep(0)
+    sleep(0)
     image = auv.get_image_bottom()
-    print(search_object(image))
+    search_object(image)
